641c8c7fee3469eb2caaae8f0d12d39993e0ab79
Aman Kumar <akgupta0524@gmail.com>
2024-09-15 10:18:08 +0530

    lopp practice

--- Loop/01_for_Loop/index.js
+++ Loop/01_for_Loop/index.js
@@ -36,7 +36,7 @@ while (j >= 1) {
 
 let k = 1;
 do {
-  console.log(k);
+  //   console.log(k);
   k++;
 } while (k <= 10);
 
@@ -44,6 +44,58 @@ do {
 
 let m = 10;
 do {
-  console.log(m);
+  //   console.log(m);
   m--;
 } while (m >= 1);
+
+/**................................................................................................ */
+
+/** Nested loops are loops inside other loops. They are often used for handling multi-dimensional data. */
+
+// Example: Multiplication Table (5x5)
+
+for (let i = 1; i <= 5; i++) {
+  let multiply = [];
+  for (let j = 1; j <= 10; j++) {
+    multiply.push(i * j);
+  }
+  //   console.log(multiply);
+}
+
+// Example: Print a star triangle pattern
+
+for (let i = 1; i <= 5; i++) {
+  let bag = "";
+  for (let j = 1; j <= i; j++) {
+    bag += "*";
+  }
+  //   console.log(bag);
+}
+
+// break
+
+// Example: Break the loop when i is 5
+
+for (let i = 0; i <= 10; i++) {
+  if (i === 5) {
+    break;
+  }
+  //   console.log(i);
+}
+
+// Continue
+/**  continue Statement
+
+The continue statement skips the current iteration and continues with the next iteration.
+ */
+
+// Example: Skip even numbers:
+
+for (let i = 1; i <= 5; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+  //   console.log(i);
+}
+
+/** Print the factorial of numbers from 1 to 5 using a loop. */
