67d104cc0246d49a8364fd4b9677c4efd1d33dd2
Aman Kumar <akgupta0524@gmail.com>
2024-09-15 09:56:45 +0530

    loop

---
+++ Loop/01_for_Loop/index.js
@@ -0,0 +1,49 @@
+/** .....................................For Loop ..................................................... */
+
+/** Example: Print numbers from 1 to 10 */
+
+for (let i = 1; i <= 10; i++) {
+  //   console.log(i);
+}
+
+/** Example: Print numbers from 10 to 1 */
+
+for (let i = 10; i >= 1; i--) {
+  //   console.log(i);
+}
+
+/** .....................................While Loop................................................... */
+
+/** Example: Print numbers from 1 to 10 */
+
+let i = 1;
+while (i <= 10) {
+  //   console.log(i);
+  i++;
+}
+
+/** Example: Print numbers from 10 to 1 */
+
+let j = 10;
+while (j >= 1) {
+  //   console.log(j);
+  j--;
+}
+
+/** ........................................Do While................................................... */
+
+/** Example: Print numbers from 1 to 10 */
+
+let k = 1;
+do {
+  console.log(k);
+  k++;
+} while (k <= 10);
+
+/** Example: Print numbers from 10 to 1 */
+
+let m = 10;
+do {
+  console.log(m);
+  m--;
+} while (m >= 1);
